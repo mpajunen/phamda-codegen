6e8d38ae4a5f4deddbb4033cde590fedce821a51
Mikael Pajunen <mikael.pajunen@gmail.com>
2015-02-19 00:06:04 +0200

    Add all, any, pick, pickAll, prop

--- src/Functions/InnerFunctions.php
+++ src/Functions/InnerFunctions.php
@@ -3,6 +3,40 @@
 namespace Phamda\Functions;
 
 /**
+ * @param callable $function
+ * @param array    $list
+ *
+ * @return bool
+ */
+function all(callable $function, array $list)
+{
+    foreach ($list as $value) {
+        if (! $function($value)) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * @param callable $function
+ * @param array    $list
+ *
+ * @return bool
+ */
+function any(callable $function, array $list)
+{
+    foreach ($list as $value) {
+        if ($function($value)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**
  * @param mixed $a
  * @param mixed $b
  *
@@ -33,6 +67,51 @@ function filter(callable $function, array $list)
 function map(callable $function, array $list)
 {
     return array_map($function, $list);
+}
+
+/**
+ * @param array $names
+ * @param array $item
+ *
+ * @return array
+ */
+function pick(array $names, array $item)
+{
+    $new = [];
+    foreach ($names as $name) {
+        if (array_key_exists($name, $item)) {
+            $new[$name] = $item[$name];
+        }
+    }
+
+    return $new;
+}
+
+/**
+ * @param array $names
+ * @param array $item
+ *
+ * @return array
+ */
+function pickAll(array $names, array $item)
+{
+    $new = [];
+    foreach ($names as $name) {
+        $new[$name] = isset($item[$name]) ? $item[$name] : null;
+    }
+
+    return $new;
+}
+
+/**
+ * @param string       $name
+ * @param array|object $object
+ *
+ * @return mixed
+ */
+function prop($name, $object)
+{
+    return is_object($object) ? $object->$name : $object[$name];
 }
 
 /**
